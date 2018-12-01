d60a18a4a09fc5fab88b7131358c22a0bfd2b580
AtiumAddict <alexandroslagios@gmail.com>
2018-12-01 14:07:59 +0200

    Made most of the level and started writing the level script

--- Assets/Scripts/Level/Level.cs
+++ Assets/Scripts/Level/Level.cs
@@ -1,18 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Level : MonoBehaviour {
 
-    public GameObject pauseMenu;
+    public Text player, txt4, txt5, txt6, txt7, txt8, tx9;
+    public RectTransform pos1, pos2, pos3;
+    private int playerNo;
+    public int startingNo;
+    public int playerPos;
+    public int counter2, counter3;
 
     void Start ()
     {
-
+        startingNo = 43;
+        playerNo = startingNo;
+        player.text = playerNo.ToString();
+        player.rectTransform.anchoredPosition = pos2.anchoredPosition;
+        playerPos = 2;
     }
 	
 	void Update ()
     {
-        
+        if (Input.GetKeyDown("right") || Input.GetKeyDown("d"))
+        {
+            if (playerPos == 3)
+            {
+                SwitchPosition(1);
+            }
+            else
+            {
+                SwitchPosition(playerPos + 1);
+            }
+        }
+    }
+
+    public void SwitchPosition (int i)
+    {
+        switch (i)
+        {
+            case 1:
+                player.rectTransform.anchoredPosition = pos1.anchoredPosition;
+                playerPos = 1;
+                break;
+            case 2:
+                player.rectTransform.anchoredPosition = pos2.anchoredPosition;
+                playerPos = 2;
+                break;
+            case 3:
+                player.rectTransform.anchoredPosition = pos3.anchoredPosition;
+                playerPos = 3;
+                break;
+            default:
+                player.rectTransform.anchoredPosition = pos2.anchoredPosition;
+                playerPos = 2;
+                break;
+        }
+    }
+
+    public int GenerateNumber ()
+    {
+        return Random.Range(0, playerNo);
     }
 }
+
